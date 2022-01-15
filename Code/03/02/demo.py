4c157a17f40d244281a923d0b0c8ab47ea3974f4
Shijie An <shijie.an1249@gmail.com>
2022-01-15 21:23:54 +0800

    updated

--- Code/03/02/demo.py
+++ Code/03/02/demo.py
@@ -1,3 +1,9 @@
 import pandas as pd
-s1=pd.Series([88,60,75])
-print(s1)
+s1=pd.Series([88,60,75], index = [1, 2, 3])
+s2 = pd.Series([20, 34, 24], index = [2, 3, 4])
+# s = s1 + s2
+# print(s)
+# # print(s1)
+print(s1[1])
+print(s1[[1, 2]])
+
